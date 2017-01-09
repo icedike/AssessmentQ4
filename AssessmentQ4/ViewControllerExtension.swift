e87fe25724fbed32bf53cbf5fc092467ec70206a
icedike <icedike@gmail.com>
2017-01-09 16:30:04 +0800

    finished the first item function

---
+++ AssessmentQ4/ViewControllerExtension.swift
@@ -0,0 +1,9 @@
+//
+//  ViewControllerExtension.swift
+//  AssessmentQ4
+//
+//  Created by LIN TINGMIN on 09/01/2017.
+//  Copyright © 2017 MarkRobotDesignTEST. All rights reserved.
+//
+
+import Foundation
